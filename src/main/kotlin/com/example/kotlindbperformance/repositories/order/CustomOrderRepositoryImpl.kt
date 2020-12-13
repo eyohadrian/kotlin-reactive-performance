21b394452695b6ca3de79a2fa270d7715e263cd9
Adrian <adrian.asensio.bdn@gmail.com>
2020-12-13 17:10:06 +0100

    insert with relationship improvement on benchmark

--- src/main/kotlin/com/example/kotlindbperformance/repositories/order/CustomOrderRepositoryImpl.kt
+++ src/main/kotlin/com/example/kotlindbperformance/repositories/order/CustomOrderRepositoryImpl.kt
@@ -2,9 +2,14 @@ package com.example.kotlindbperformance.repositories.order
 
 import com.example.kotlindbperformance.entities.Order
 import io.r2dbc.spi.Connection
+import kotlinx.coroutines.reactive.collect
 import org.springframework.data.r2dbc.core.DatabaseClient
 import org.springframework.transaction.annotation.Transactional
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toFlux
+import reactor.kotlin.core.publisher.toMono
+import java.util.stream.Collectors
 
 open class CustomOrderRepositoryImpl(private val databaseClient: DatabaseClient): CustomOrderRepository {
 /*
@@ -54,11 +59,13 @@ open class CustomOrderRepositoryImpl(private val databaseClient: DatabaseClient)
             .into("order_user")
             .value("user_id", order.user)
             .value("order_id", order.id).fetch().rowsUpdated())
-        .then(databaseClient.insert()
+        .then(order.products.map { product -> databaseClient.insert()
             .into("order_product")
             .value("order_id", order.id)
-            .value("product_id", order.products.first())
-            .fetch().rowsUpdated())
+            .value("product_id", product)
+            .fetch().rowsUpdated()}
+            .fold(Flux.empty<Int>()) {acc, mono -> acc.mergeWith(mono)}
+            .reduce{ a, b -> a + b})
 
     }
 }
